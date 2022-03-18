3f1921026174cb98fd6f44bba204de44a62e3e4d
yigitsahin <yigit.sahin@denk-it.com>
2022-03-18 15:51:07 +0300

    ignore file and main project is implemented

---
+++ src/main/java/com/competitiongame/competitiongame/CompetitionGameApplication.java
@@ -0,0 +1,13 @@
+package com.competitiongame.competitiongame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/competitiongame/competitiongame/CompetitionGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.competitiongame.competitiongame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
