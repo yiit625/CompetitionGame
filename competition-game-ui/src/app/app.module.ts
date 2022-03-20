a9de0385f4b8bed71025d144caac8fb6871af7a1
yigitsahin <yigit.sahin@denk-it.com>
2022-03-20 09:45:15 +0300

    Submission Page design and form is okey

--- competition-game-ui/src/app/app.module.ts
+++ competition-game-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {DevUIModule} from "ng-devui";
+import {DevUIModule, FormModule} from "ng-devui";
 import { GridPageComponent } from './grid-page/grid-page.component';
 import { SubmitPageComponent } from './submit-page/submit-page.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SubmitPageComponent } from './submit-page/submit-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DevUIModule
+    DevUIModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- competition-game-ui/src/app/submit-page/submit-page.component.ts
+++ competition-game-ui/src/app/submit-page/submit-page.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormLayout } from 'ng-devui/form';
 
 @Component({
   selector: 'app-submit-page',
   templateUrl: './submit-page.component.html',
-  styleUrls: ['./submit-page.component.css']
+  styleUrls: ['./submit-page.component.scss']
 })
 export class SubmitPageComponent implements OnInit {
-
-  constructor() { }
-
+  layoutDirection: FormLayout = FormLayout.Horizontal;
+  constructor() {
+  }
   ngOnInit(): void {
   }
+
+  selectOptions = [{
+    id: 1,
+    label: 'Option1'
+  },
+    {
+      id: 2,
+      label: 'Option2'
+    },
+    {
+      id: 3,
+      label: 'Option3'
+    }];
+
+  formData = {
+    inputValue: '',
+    textareaValue: '',
+    script: '',
+    selectValue: this.selectOptions[1]
+  };
+
+  submit() {
+    console.log(this.formData)
+  }
+
 
 }
